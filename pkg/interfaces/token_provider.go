ef9c6e3b9346e53cc851b00425135a684f10f605
deejcoder <deejtonks@gmail.com>
2022-07-01 16:40:34 +1200

    progress

--- pkg/interfaces/token_provider.go
+++ pkg/interfaces/token_provider.go
@@ -3,12 +3,13 @@ package interfaces
 type TokenProvider string
 
 const (
-	Google TokenProvider = "GOOGLE"
+	Google   TokenProvider = "GOOGLE"
+	SafeSend TokenProvider = "SAFE_SEND"
 )
 
 // Values returns a list of valid Enum values
 func (TokenProvider) Values() (kinds []string) {
-	for _, s := range []TokenProvider{Google} {
+	for _, s := range []TokenProvider{Google, SafeSend} {
 		kinds = append(kinds, string(s))
 	}
 	return
--- pkg/services/auth/auth.go
+++ pkg/services/auth/auth.go
@@ -10,7 +10,41 @@ import (
 	"time"
 )
 
+const (
+	CtxAccessTokenKey    = "AccessToken"
+	CtxRefreshTokenKey = "RefreshToken"
+	CtxTokenExpiryKey  = "TokenExpiry"
+)
 func Ping(conn *grpc.ClientConn, p *pb.Request, audience string) (*pb.Response, error) {
+
+	md, ok := grpcMetadata.FromIncomingContext(conn.)
+
+	client := pb.NewAuthenticationClient(conn)
+	return client.Ping(outgoingCtx, p)
+}
+
+func ValidateToken(ctx context.Context, p *pb.Request) bool {
+	md, ok := grpcMetadata.FromIncomingContext(ctx)
+	if !ok { return false }
+
+	expiry := md.Get(CtxTokenExpiryKey)[0]
+	expiryTime, err := time.Parse(time.RFC3339, expiry)
+	if err != nil { return false }
+
+	if expiryTime
+
+	acessToken := md.Get(CtxAccessTokenKey)
+
+
+
+}
+
+func RefreshToken(ctx context.Context, p *pb.Request) bool {
+
+
+}
+
+func RequestToken(conn *grpc.ClientConn, p *pb.Request, audience string) (*pb.Response, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
@@ -25,11 +59,12 @@ func Ping(conn *grpc.ClientConn, p *pb.Request, audience string) (*pb.Response, 
 	}
 
 	meta := grpcMetadata.Pairs(
-		"authorization", "Bearer"+token.AccessToken,
-		"token_expiry", token.Expiry.String())
+		CtxAccessTokenKey, token.AccessToken,
+		CtxTokenExpiryKey, token.Expiry.String(),
+		CtxRefreshTokenKey, token.RefreshToken)
 
 	outgoingCtx := grpcMetadata.NewOutgoingContext(ctx, meta)
 
 	client := pb.NewAuthenticationClient(conn)
-	return client.Ping(outgoingCtx, p)
+	//return client.RequestToken(outgoingCtx, p)
 }
